b2bf119390fd57c3a3f0af2f16c177796bb2604e
Lucas Jandrew <fapiko@gmail.com>
2022-01-06 15:53:37 -0500

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,62 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Link as RouterLink, Route, Routes} from "react-router-dom";
+import { SignUp } from "./routes/signup";
+import {AppBar, CssBaseline, GlobalStyles, ThemeProvider, Toolbar} from "@mui/material";
+import {createTheme} from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+
+import Button from "@mui/material/Button";
+import Link from "@mui/material/Link";
+import {Login} from "./routes/login";
+
+const theme = createTheme();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <BrowserRouter>
+              <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
+              <CssBaseline />
+              <AppBar position="static"
+                      color="default"
+                      elevation={0}
+                      sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}>
+                  <Toolbar sx={{ flexWrap: 'wrap' }}>
+                      <Link component={RouterLink} underline="none" color="text.primary" to="/" noWrap sx={{ flexGrow: 1 }}>
+                          <Typography variant="h6" color="inherit" >
+                              John Hancock
+                          </Typography>
+                      </Link>
+                      <nav>
+                          <Button
+                              variant="outlined"
+                              component={RouterLink}
+                              to="/users/login"
+                              sx={{ my: 1, mx: 1.5 }}
+                          >
+                              Login
+                          </Button>
+                          <Button
+                              component={RouterLink}
+                              variant="outlined"
+                                to="/users/signup"
+                                sx={{ my: 1, mx: 1.5 }}>
+                              Sign up
+                          </Button>
+                      </nav>
+                  </Toolbar>
+              </AppBar>
+
+              <Routes>
+                  <Route path="/" element={<App />} />
+                  <Route path='users/login' element={<Login />} />
+                  <Route path="users/signup" element={<SignUp />} />
+              </Routes>
+          </BrowserRouter>
+      </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +66,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
---
+++ src/routes/login.tsx
@@ -0,0 +1,87 @@
+import {Component} from "react";
+import Container from '@mui/material/Container';
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+import {Checkbox, FormControlLabel, TextField} from "@mui/material";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
+export class Login extends Component {
+    handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+
+        console.log(data);
+    }
+
+    render() {
+        return (
+            <Container component="main" maxWidth="xs">
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box component="form" onSubmit={this.handleSubmit} noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
+        );
+    }
+}
